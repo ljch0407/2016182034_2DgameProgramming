4864106aba2619bdf5e453bc4db233d7e160dad5
ljch0407 <70691135+ljch0407@users.noreply.github.com>
2020-11-21 15:09:21 +0900

    bgm추가

--- 최종프로젝트/main_state.py
+++ 최종프로젝트/main_state.py
@@ -1,12 +1,23 @@
 from pico2d import *
 import gfw
+import gobj
 from player import Player
+from background import HorzScrollBackground
 
 def enter():
     gfw.world.init(['bg','enemy','player'])
     global player
     player = Player()
     gfw.world.add(gfw.layer.player,player)
+    bg = HorzScrollBackground("bg.png")
+    bg.speed=20
+    gfw.world.add(gfw.layer.bg,bg)
+
+    global bg_music
+    bg_music = load_music(gobj.res('bg.mp3'))
+    bg_music.set_volume(60)
+   
+    bg_music.repeat_play()
     
     pass
 
