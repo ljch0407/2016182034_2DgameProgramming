0e36b87a4c4530d2e9a8e19304cc1bad3c40d0eb
ljch0407 <70691135+ljch0407@users.noreply.github.com>
2020-11-28 14:03:51 +0900

    오브젝트 Generator추가

---
+++ 최종프로젝트/generator.py
@@ -0,0 +1,32 @@
+from pico2d import *
+import gfw
+from enemy import *
+import random
+
+enemy_count =10
+
+def update(score):
+    Max_enemy_count = enemy_count + score/10
+    if gfw.world.count_at(gfw.layer.enemy)<Max_enemy_count:
+        generate_enemy(score)
+
+
+def generate_enemy(score):
+    x,y,dx,dy = get_coords(score)
+    e = Enemy((x,y), (dx,dy))
+    gfw.world.add(gfw.layer.enemy,m)
+
+def get_coords(score):
+    dx = random.random()
+    dx -= dx*2
+
+    dy =0
+
+    mag = 1+score/60
+    dx *=mag
+    dy *=mag
+
+    x= get_canvas_width()
+    y = random.uniform(get_canvas_height()//4+80,get_canvas_height())
+
+    return x,y,dx,dy
